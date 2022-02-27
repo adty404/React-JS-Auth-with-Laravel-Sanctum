c663c122067c2980b6e73d9e43798bccf55af7cf
Aditya Prasetyo <adty404@gmail.com>
2022-02-27 19:55:47 +0700

    Endpoint Login

--- backend.laravel/app/Http/Controllers/Auth/LoginController.php
+++ backend.laravel/app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -15,6 +19,21 @@ class LoginController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::whereEmail($request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ["The credentials are incorrect"],
+            ]);
+        }
+
+        $user->tokens()->delete();
+        $token = $user->createToken('web-token')->plainTextToken;
+        return (new UserResource($user))->additional(compact('token'));
     }
 }
--- backend.laravel/routes/api.php
+++ backend.laravel/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\MeController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', RegisterController::class);
+Route::post('/login', LoginController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('me', MeController::class);
+    Route::post('logout', LogoutController::class);
+});
