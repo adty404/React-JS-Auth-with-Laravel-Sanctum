02b422727fb24b8affa7c46a7a5c8685a2cccfcd
Aditya Prasetyo <adty404@gmail.com>
2022-03-02 06:44:55 +0700

    Global Config Axios

--- front.reactjs/src/index.js
+++ front.reactjs/src/index.js
@@ -8,6 +8,10 @@ import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
 
 axios.defaults.baseURL = "http://127.0.0.1:8000/api"
+axios.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('tokenUser')}`
+    return config
+})
 
 ReactDOM.render(
     <StrictMode>
